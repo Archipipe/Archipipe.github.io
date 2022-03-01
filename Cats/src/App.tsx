60b8341d777d4a0c29547cc36d113ac13bf38ea0
Archipipe <bob333223@mail.ru>
2022-03-01 23:14:51 +0300

    if you have something you want to Keep comment

---
+++ Cats/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './HeaderStuff/Header';
+import Topic from './TopicStuff/Topic';
+
+
+function App() {
+  return (
+    <><Header /><Topic /></>
+  );
+}
+
+export default App;
